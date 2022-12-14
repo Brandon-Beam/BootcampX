def0570e466ebaffd1be3d4064966f94734df666
Brandon <bbeam1@gmail.com>
2022-12-13 19:12:27 -0700

    more sql queries practice

---
+++ 4_queries/9_avg_total_duration.sql
@@ -0,0 +1,7 @@
+SELECT AVG(total_duration) as average_total_duration
+FROM (SELECT sum(completed_at-started_at) as total_duration, cohorts.name as name
+FROM assistance_requests
+JOIN students on student_id = students.id
+JOIN cohorts on cohort_id = cohorts.id
+GROUP BY cohorts.name
+ORDER BY total_duration) as total_duration;
