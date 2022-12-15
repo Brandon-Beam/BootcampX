695364dbf0a08507401ea9b92b9729eb749572df
Brandon <bbeam1@gmail.com>
2022-12-14 23:17:34 -0700

    basic sql injection protection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,16 +8,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-
-pool.query(`
-SELECT distinct teachers.name as teacher, cohorts.name as cohort
+const queryString = `SELECT distinct teachers.name as teacher, cohorts.name as cohort
 FROM assistance_requests
 JOIN teachers ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2]}';
-`)
+WHERE cohorts.name = $1;
+`;
+const cohortName = process.argv[2] || 'JUL02';
+// Store all potentially malicious values in an array.
+const values = [cohortName];
+
+pool.query(queryString, values)
   .then(res => {
     console.log(res.rows);
   })
   .catch(err => console.error('query error', err.stack));
+
